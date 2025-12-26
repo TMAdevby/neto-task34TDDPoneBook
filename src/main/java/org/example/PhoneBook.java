8a607ba8bec3c5ceef6c9bb41ee361d4c6859651
TMAdevby <mtipun@mail.ru>
2025-12-26 15:57:00 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,21 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    Map<String,String> book = new HashMap<>();
+
+    public boolean add(String name, String number) {
+        if (book.containsKey(name)) {
+            return false;
+        }
+//        book.put(name, number);
+        return false;
+    }
+
+    public String get(String name) {
+        return book.get(name);
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,35 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+
+    PhoneBook phoneBook;
+
+    @BeforeEach
+    void setUp(){
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    void shouldAddNumberSuccessfully() {
+
+        boolean added = phoneBook.add("Alex","+911234567");
+
+        assertTrue(added, "Contact should be added successfully");
+        assertEquals("+911234567", phoneBook.get("Alex"));
+    }
+
+    @Test
+    void shouldDontAddDuplicate() {
+
+        phoneBook.add("Alex","+911234567");
+
+        boolean added = phoneBook.add("Alex","+777777777");
+
+        assertFalse(added, "Contact shouldn't be added successfully");
+        assertEquals("+911234567", phoneBook.get("Alex"));
+    }
 }
